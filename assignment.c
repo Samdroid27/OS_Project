e87addb76dd13c1f3f7cb40380914202cf1eecd0
Shivam Agrawal <shivam.agr2704@gmail.com>
2021-11-11 14:53:45 +0530

    Organized code

--- assignment.c
+++ assignment.c
@@ -13,19 +13,16 @@
 #define READ 0
 #define WRITE 1
 
-int main(){
- pid_t child_c1,child_c2,child_c3;
- int pfd1[2];
- int p1 = pipe(pfd1);
- child_c1 = fork();
-//input from user : choice of algo,n1,n2,n3 and communicate n1 to c1, n2 to c2 , n3 to c3 using pipes
-//shared memory setup
-
-    if(child_c1 == 0)
+void roundrobin(int n1,int n2,int n3){
+	pid_t child_c1,child_c2,child_c3;
+ 	int pfd1[2];
+ 	int p1 = pipe(pfd1);
+ 	child_c1 = fork();
+	if(child_c1 == 0)
         { //Child_c1 process
           //create 2 threads here  
         }
-    else
+    	else
         { //Parent process
         
             int pfd2[2];
@@ -39,25 +36,92 @@ int main(){
                 }
                 else
                 { //Parent process
-                    int pfd3[2];
-                    int p3 = pipe(pfd3);
-                    child_c3 = fork();
-                        if(child_c3 == 0)
-                        { //Child_c3 process
+                    	int pfd3[2];
+                    	int p3 = pipe(pfd3);
+                    	child_c3 = fork();
+                       if(child_c3 == 0)
+                       { //Child_c3 process
                           //create 2 threads here  
 
-                        }
-                        else
-                        {
+                       }
+                       else
+                       {
                             //parent process
                             wait(NULL);
                             wait(NULL);
                             wait(NULL);
-                        }
+                       }
 
 
                         
 
                 }
         }
+}
+
+void fcfs(int n1,int n2,int n3){
+	pid_t child_c1,child_c2,child_c3;
+ 	int pfd1[2];
+ 	int p1 = pipe(pfd1);
+ 	child_c1 = fork();
+	if(child_c1 == 0)
+        { //Child_c1 process
+          //create 2 threads here  
+        }
+    	else
+        { //Parent process
+        
+            int pfd2[2];
+            int p2 = pipe(pfd2);
+            child_c2 = fork();
+                if(child_c2 == 0)
+                { //Child_c2 process
+                  //create 2 threads here  
+
+
+                }
+                else
+                { //Parent process
+                    	int pfd3[2];
+                    	int p3 = pipe(pfd3);
+                    	child_c3 = fork();
+                       if(child_c3 == 0)
+                       { //Child_c3 process
+                          //create 2 threads here  
+
+                       }
+                       else
+                       {
+                            //parent process
+                            wait(NULL);
+                            wait(NULL);
+                            wait(NULL);
+                       }
+
+
+                        
+
+                }
+        }
+
+}
+
+void work(int scheduling,int n1,int n2,int n3){
+if(scheduling == 1) roundrobin(n1,n2,n3);
+else fcfs(n1,n2,n3);
+}
+
+
+int main(){
+//input from user : choice of algo,n1,n2,n3
+//shared memory setup
+int scheduling;
+printf("Choose desired scheduling algorithm :\n");
+printf("Enter 1 for Round Robin\n");
+printf("Enter 2 for FCFS\n");
+scanf("%d",&scheduling);
+int n1,n2,n3;
+printf("Enter workload for child processes N1,N2,N3\n");
+scanf("%d%d%d",&n1,&n2,&n3);
+work(scheduling,n1,n2,n3);    
 }
