44111feca8a4ef40472b71f27fadbf6188d065b7
manavroX <manav.bhagchandani3@gmail.com>
2021-11-12 18:41:37 +0530

    fcfs done

--- os_assig.c
+++ os_assig.c
@@ -26,74 +26,136 @@ pthread_mutex_t mutex3 = PTHREAD_MUTEX_INITIALIZER;
 
 //bool flags to tell monitor when to allow computaion
 bool *shmPtr; 
+int l1=1,l2=1,l3=1;
 
-void *monitor_c1(){
-	pthread_mutex_lock(&mutex1);
-	 //if(shmPtr[0] && !shmPtr[1]) then child 1 runs
-        while(!(shmPtr[0] && !shmPtr[1]))
-        {     
-            	usleep(1000);
-        }
-        pthread_cond_signal(&cond1);//give timestamp also
-	pthread_mutex_unlock(&mutex1);
-}
+// void *monitor_c1(){
+//     printf("inside monitor c1\n");
+// 	pthread_mutex_lock(&mutex1);
+// 	 //if(shmPtr[0] && !shmPtr[1]) then child 1 runs
+//     while(!(shmPtr[0] && !shmPtr[1]))
+//     {     
+//         usleep(20);
+//     }
+//     printf("inside monitor c1 after while\n");
+    
+//     pthread_cond_signal(&cond1);//give timestamp also
+//     printf("after cond_signal c1\n");
+//     pthread_mutex_unlock(&mutex1);
+//     printf("after mutex unlock c1\n");
+	
+// }
 
-void *monitor_c2(){
-	pthread_mutex_lock(&mutex2);
-		//if(!shmPtr[0] && shmPtr[1]) then child 2 runs
-        while(!(!shmPtr[0] && shmPtr[1]))
-        {  
-            	usleep(1000);
-        }
-         pthread_cond_signal(&cond2); //give timestamp also
-		 pthread_mutex_unlock(&mutex2);
+// void *monitor_c2(){
+//     printf("inside monitor c2\n");
+// 	pthread_mutex_lock(&mutex2);
+// 		//if(!shmPtr[0] && shmPtr[1]) then child 2 runs
+//     while(!(!shmPtr[0] && shmPtr[1]))
+//     {  
+//             usleep(20);
+//     }
+//     printf("inside monitor c2 after while\n");
+//     pthread_cond_signal(&cond2); //give timestamp also
+//     pthread_mutex_unlock(&mutex2);
  
-}
+// }
 
-void *monitor_c3(){
-	pthread_mutex_lock(&mutex3);
-//if(!shmPtr[0] && !shmPtr[1]) then child 3 runs
-    while(!(!shmPtr[0] && !shmPtr[1]))         
-    {   
-	    usleep(1000);
+// void *monitor_c3(){
+//     printf("inside monitor c3\n");
+// 	pthread_mutex_lock(&mutex3);
+// //if(!shmPtr[0] && !shmPtr[1]) then child 3 runs
+//     while(!(!shmPtr[0] && !shmPtr[1]))         
+//     {   
+// 	    usleep(20);
   
-    }
-	pthread_cond_signal(&cond3);//give timestamp also
-	pthread_mutex_unlock(&mutex3);
+//     }
+//     printf("inside monitor c3 after while\n");
+// 	pthread_cond_signal(&cond3);//give timestamp also
+// 	pthread_mutex_unlock(&mutex3);
 
-}
+// }
 
 void *add_nums(void *p){
+    // printf("inside compute c1\n");
  	int *param = (int *)p;
  	pthread_mutex_lock(&mutex1);
- 	//while(!condition)
-		pthread_cond_wait(&cond1, &mutex1);
-	//do_addition of numbers from 1 to n1
-	// when done,return result to parent process via pipe1
+    if(l1==1)
+    {
+        l1 = 2;
+        pthread_cond_wait(&cond1, &mutex1);
+        printf("Child 1 running\n");
+        printf("Child1 completed\n");
+    }
+    else
+    {
+        //Child 1 montior thread
+        while(!(shmPtr[0] && !shmPtr[1]))
+        {     
+            usleep(20);
+        }
+        pthread_cond_signal(&cond1);
+        printf("monitor thread 1 completed\n");
+    }
 
 	pthread_mutex_unlock(&mutex1);
- 	
+
+    // printf("child 1 completed\n");
 }
 
 void *read_nums(void *p){
+    // printf("inside compute c2\n");
  	int *param = (int *)p;
  	pthread_mutex_lock(&mutex2);
 	//while(!condition)
-		pthread_cond_wait(&cond2, &mutex2);
+    if(l2==1)
+    {
+        l2 = 2;
+        pthread_cond_wait(&cond2, &mutex2);
+        printf("Child 2 running\n");
+        printf("Child 2 completed\n");
+    }
+    else
+    {
+        //Child 2 montior thread
+        while(!(!shmPtr[0] && shmPtr[1]))
+        {  
+                usleep(20);
+        }
+        pthread_cond_signal(&cond2);
+        printf("monitor thread 2 completed\n");
+    }
+    
 	//do_reading of n2 numbers from input file
 	//when done,print done_reading on the console
 	pthread_mutex_unlock(&mutex2);
- 	
+ 	// printf("Child 2 completed\n");
 }
 
 void *read_add_nums(void *p){
- 	int *param = (int *)p;
+    int *param = (int *)p;
  	pthread_mutex_lock(&mutex3);
 	//while(!condition)
-		pthread_cond_wait(&cond3, &mutex3);
-	//do_reading of n3 numbers from input file and add them 
-	// when done,return result to parent process via pipe3
+    if(l3==1)
+    {
+        l3 = 2;
+        pthread_cond_wait(&cond3, &mutex3);
+        printf("Child 3 running\n");
+        printf("Child 3 completed\n");
+    }
+    else
+    {
+        //Child 3 montior thread
+        while(!(!shmPtr[0] && !shmPtr[1]))
+        {  
+                usleep(20);
+        }
+        pthread_cond_signal(&cond3);
+        printf("monitor thread 3 completed\n");
+    }
+    
+	//do_reading of n2 numbers from input file
+	//when done,print done_reading on the console
 	pthread_mutex_unlock(&mutex3);
+ 	
  	
 }
 
@@ -151,6 +213,10 @@ void roundrobin(int n1,int n2,int n3){
 }
 
 void fcfs(int n1,int n2,int n3){
+
+    // printf("inside fcfs function\n");
+
+    pthread_mutex_init(&mutex1,NULL);
     
     int shmid; 
     //int n;
@@ -173,7 +239,9 @@ void fcfs(int n1,int n2,int n3){
 	if(child_c1 == 0)
     { 
         //Child_c1 process
+        
         sleep(1);
+        // printf("child1\n");
         if( (shmid = shmget(2041, 32, 0)) == -1 )
         {
             perror("error in shmget in child1");
@@ -186,11 +254,12 @@ void fcfs(int n1,int n2,int n3){
             exit(2);
         }
         
-         
-         pthread_create(&monitor_thread[0], &at[0], monitor_c1, NULL);
-         pthread_create(&computation_thread[0], &at[0], add_nums, n1);
-         pthread_join(computation_thread[0], NULL);
-         pthread_join(monitor_thread[0], NULL);
+        // printf("before thread creation c1\n");
+        pthread_create(&monitor_thread[0], &at[0], add_nums, n1);
+        pthread_create(&computation_thread[0], &at[0], add_nums, n1);
+        // printf("after thread creation c1\n");
+        pthread_join(computation_thread[0], NULL);
+        pthread_join(monitor_thread[0], NULL);
 
        
 
@@ -204,7 +273,9 @@ void fcfs(int n1,int n2,int n3){
             if(child_c2 == 0)
             { 
                 //Child_c2 process
+                
                 sleep(1);
+                // printf("child2\n");
                 if( (shmid = shmget(2041, 32, 0)) == -1 )
                 {
                     perror("error in shmget in child1");
@@ -216,10 +287,12 @@ void fcfs(int n1,int n2,int n3){
                     perror("error in shmat in child1");
                     exit(2);
                 }
-                 pthread_create(&monitor_thread[1], &at[1], monitor_c2, NULL);
-                 pthread_create(&computation_thread[1], &at[1], read_nums, n1);
-                 pthread_join(computation_thread[1], NULL);
-         	     pthread_join(monitor_thread[1], NULL);
+                // printf("before thread creation c2\n");
+                pthread_create(&monitor_thread[1], &at[1], read_nums, n2);
+                pthread_create(&computation_thread[1], &at[1], read_nums, n2);
+                // printf("after thread creation c2\n");
+                pthread_join(computation_thread[1], NULL);
+                pthread_join(monitor_thread[1], NULL);
                 
 
                
@@ -233,7 +306,9 @@ void fcfs(int n1,int n2,int n3){
                 if(child_c3 == 0)
                 {   
                     //Child_c3 process
+                    
                     sleep(1);
+                    // printf("child3\n");
                     if( (shmid = shmget(2041, 32, 0)) == -1 )
                     {
                         perror("error in shmget in child1");
@@ -245,9 +320,12 @@ void fcfs(int n1,int n2,int n3){
                         perror("error in shmat in child1");
                         exit(2);
                     }
-                    pthread_create(&monitor_thread[2], &at[2], monitor_c3, NULL);
+
+                    // printf("before thread creation c2\n");
+
+                    pthread_create(&monitor_thread[2], &at[2], read_add_nums, n3);
 					pthread_create(&computation_thread[2], &at[2], read_add_nums, n3);
-                	
+                	// printf("after thread creation c3\n");
                    
 					pthread_join(computation_thread[2], NULL);
          			pthread_join(monitor_thread[2], NULL);
@@ -256,24 +334,27 @@ void fcfs(int n1,int n2,int n3){
                 else
                 {
                     //parent process
+                    printf("parent\n");
 
                     if( (shmid = shmget(2041, 32, 0666 | IPC_CREAT)) == -1 )
                     {
                         perror("error in shmget in parent");
                         exit(1);
                     }
+                    // printf("workd till here\n");
                     shmPtr = shmat(shmid, 0, 0);
                     if (shmPtr == (bool *) -1)
                     {
                         perror("error in shmat in parent");
                         exit(2);
                     }
-
+                    // printf("works till here\n");
+                    printf("Parent writing\n");
                     //fcfs scheduling here
-                    printf("Parent writing");
+                    
                     shmPtr[0] = true;
                     shmPtr[1] = false;
-                    
+                    // printf("over here\n");
                     wait(NULL);
 
                     shmPtr[0] = false;
